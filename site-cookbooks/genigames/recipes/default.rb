870754b07a0d399b5e08308a7bb96089636384dd
Richard Klancer <rpk@pobox.com>
2012-04-19 12:39:28 -0400

    GeniGames Apache proxy & docroot setup

--- site-cookbooks/genigames/recipes/default.rb
+++ site-cookbooks/genigames/recipes/default.rb
@@ -9,4 +9,20 @@ file "/var/www/index.html" do
   group "root"
   mode "777"
 end
-  
+
+# set up resources server w/ proxy server to all of the rest
+include_recipe "apache2::mod_proxy"
+include_recipe "apache2::mod_proxy_http"
+
+include_recipe "apache2::disable_default_site"
+
+web_app "genigames-apache" do
+  server_name "genigames.dev.concord.org"
+  cookbook "apache2"
+  docroot "/var/www/static"
+  is_default true
+  proxies [
+    {:path => "/biologica/", :remote => "http://geniverse.dev.concord.org/biologica/"},
+    {:path => "/resources/", :remote => "http://geniverse.dev.concord.org/resources/"}
+  ]
+end
