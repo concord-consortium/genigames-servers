e6625746ef37857d1c6b0ebde83f276bd6070e0f
Richard Klancer <rpk@pobox.com>
2012-04-24 13:42:59 -0400

    Remove apache2 from genigames.dev

--- site-cookbooks/genigames/recipes/default.rb
+++ site-cookbooks/genigames/recipes/default.rb
@@ -4,29 +4,25 @@ directory "/var/www" do
   mode "777"
 end
 
-file "/var/www/index.html" do
-  owner "deploy"
-  group "root"
-  mode "777"
-end
+# The following is no longer used, but is left for reference b/c that's how the Apache *files*
+# got onto genigames.dev.concord.org
 
 # set up resources server w/ proxy server to all of the rest
-include_recipe "apache2::mod_proxy"
-include_recipe "apache2::mod_proxy_http"
+# include_recipe "apache2::mod_proxy"
+# include_recipe "apache2::mod_proxy_http"
 
-include_recipe "apache2::disable_default_site"
+# include_recipe "apache2::disable_default_site"
 
-web_app "genigames-apache" do
-  server_name "genigames.dev.concord.org"
-  cookbook "apache2"
-  docroot "/var/www/static"
-  is_default true
-  proxies [
-    {:path => "/biologica/", :remote => "http://geniverse.dev.concord.org/biologica/"},
-    {:path => "/resources/", :remote => "http://geniverse.dev.concord.org/resources/"}
-  ]
-end
-
+# web_app "genigames-apache" do
+#   server_name "genigames.dev.concord.org"
+#   cookbook "apache2"
+#   docroot "/var/www/static"
+#   is_default true
+#   proxies [
+#     {:path => "/biologica/", :remote => "http://geniverse.dev.concord.org/biologica/"},
+#     {:path => "/resources/", :remote => "http://geniverse.dev.concord.org/resources/"}
+#   ]
+# end
 
 # perhaps these should just be put in the runlist instead??
 
